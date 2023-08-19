d3a92039fa876dfac4c4f40bef3923cf27429b55
seunggyu <121854467+BA3KH0@users.noreply.github.com>
2023-08-19 15:35:55 +0900

    main.py

--- main.py
+++ main.py
@@ -4,12 +4,11 @@ from fastapi import FastAPI, Request
 import torchvision.transforms as transforms
 from PIL import Image
 from string import punctuation
-# text preprocessing modules
 from nltk.tokenize import word_tokenize
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-import re  # regular expression
+import re  
 import os
 import joblib
 from os.path import dirname, join, realpath
@@ -33,22 +32,22 @@ def text_preprocessing(text, remove_stop_words=True, lemmatize_words=True):
     text = re.sub(r"\'s", " ", text)
     text = re.sub(r"http\S+", " link ", text)
     text = re.sub(r"\b\d+(?:\.\d+)?\s+", "", text)  # remove numbers
-    # Remove punctuation from text
+
     text = "".join([c for c in text if c not in punctuation])
-    # Optionally, remove stop words
+
     if remove_stop_words:
         # load stopwords
         stop_words = stopwords.words("english")
         text = text.split()
         text = [w for w in text if not w in stop_words]
         text = " ".join(text)
-    # Optionally, shorten words to their stems
+
     if lemmatize_words:
         text = text.split()
         lemmatizer = WordNetLemmatizer()
         lemmatized_words = [lemmatizer.lemmatize(word) for word in text]
         text = " ".join(lemmatized_words)
-    # Return a list of words
+
     return text
 
 
@@ -63,10 +62,8 @@ def predict_sentiment(text: str):
     probas = model.predict_proba([preprocessed_text])
     output_probability = "{:.2f}".format(float(probas[:, output]))
 
-    # output dictionary
-    sentiments = {0: "Negative", 1: "Positive"}
+    sentiments = {0: "Neg", 1: "Pos"}
 
-    # show results
     result = {"prediction": sentiments[output], "Probability": output_probability}
     return result
 
