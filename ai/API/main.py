d115e96b0445c01efe102309046fba007f5c3aa5
seung <seunggyuh@jbnu.ac.kr>
2023-08-29 21:47:53 +0900

    API risk

--- ai/API/main.py
+++ ai/API/main.py
@@ -14,7 +14,7 @@ model2 = models.efficientnet_b2(num_classes=3)
 # 모델 가중치 파일 로드
 device = torch.device('cpu')
 
-model1.load_state_dict(torch.load('normal_classification_64epoch.pth', map_location=device))
+model1.load_state_dict(torch.load('normal_classification_48epoch.pth', map_location=device))
 model1.eval()
 
 model2.load_state_dict(torch.load('0826_09_1epoch.pth', map_location=device))
@@ -42,6 +42,8 @@ def preprocess_image2(image):
     return transform(image).unsqueeze(0)
 
 
+k=0.15
+
 # 이미지 업로드와 추론을 처리하는 엔드포인트
 @app.post("/predict/")
 async def predict(file: UploadFile):
@@ -52,6 +54,8 @@ async def predict(file: UploadFile):
         outputs1 = model1(preprocessed_image1)
 
     probabilities1 = torch.nn.functional.softmax(outputs1[0], dim=0)
+    inner = probabilities1 * torch.tensor([1.0, 0.0, 0.5])
+    risk = torch.sum(inner)
     predicted_class1 = torch.argmax(probabilities1).item()
 
     # 클래스 인덱스에 따른 클래스 이름 설정
@@ -64,15 +68,40 @@ async def predict(file: UploadFile):
             outputs2 = model2(preprocessed_image2)
 
         predicted_class2 = torch.argmax(outputs2).item()
-        probabilities2= torch.nn.functional.softmax(outputs2[0], dim=0)
-        class_names2 = ["class1", "class2", "class3"]
-        predicted_class2 = class_names2[predicted_class2]
 
-        return {'상태': class_names[predicted_class1], "predicted_class": predicted_class2, 'prob': probabilities2.tolist()}
+
+        probabilities2= torch.nn.functional.softmax(outputs2[0], dim=0)
+
+        # 가장 큰 값의 인덱스
+        max_value = max(probabilities2)
+        max_index = torch.argmax(probabilities2)
+
+        # 가장 큰 값을 제외한 나머지 값들과 인덱스
+        other_values = probabilities2[torch.arange(len(probabilities2)) != max_index]
+        other_indices = torch.arange(len(probabilities2))[torch.arange(len(probabilities2)) != max_index]
+
+        # 가장 큰 값과의 차이
+        max_difference = max_value - torch.max(other_values)
+
+        # 조건을 충족하는 인덱스 추출
+        selected_indices = [max_index.item()]
+
+        # 반복문을 통해 조건을 만족하는 인덱스 찾기
+        for idx, value in zip(other_indices, other_values):
+            if max_value - value < k:
+                selected_indices.append(idx.item())
+
+
+
+        class_names2 = ["철근노출", "박리", "콘크리트"]
+        selected_class_names = [class_names2[idx] for idx in selected_indices]       # k값에 따라 여러개 추출될 수 있는 class
+        predicted_class2 = class_names2[predicted_class2]                           # 확률이 가장 높은 값 하나만 추출
+
+        return {'상태': class_names[predicted_class1], "predicted_class": selected_class_names, 'prob': probabilities2.tolist(), '위험도': risk.item()}
 
     else:
 
-        return {'상태': class_names[predicted_class1]}
+        return {'상태': class_names[predicted_class1], '위험도': risk.item()}
 
 
 if __name__ == "__main__":
